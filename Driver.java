1618534f09dd544b1b5d730c2585cf071d5c64a9
Kartik Gupta <thisiskartikgupta@gmail.com>
2022-08-03 15:23:23 +0530

    Fixed getRangeSum() in SegmentTree

--- Driver.java
+++ Driver.java
@@ -1,12 +1,15 @@
 import SegmentTreeExceptions.SegmentTreeOutOfBoundsException;
 import SegmentTreeExceptions.SizeLimitException;
+
 public class Driver {
 
     public static void main(String[] args) throws SizeLimitException, SegmentTreeOutOfBoundsException {
 
-        SegmentTree segmentTree = new SegmentTree(new int[] {11, 22, 33, 44, 55});
+        SegmentTree segmentTree = new SegmentTree(new int[]{11, 22, 33, 44, 55});
         segmentTree.display();
-        System.out.println(segmentTree.getRangeSum(0,1));
+//        segmentTree.update(1,200);
+//        segmentTree.display();
+        System.out.println(segmentTree.getRangeSum(2, 3));
 //        segmentTree.displayRAW();
 //        System.out.println(segmentTree.getRangeSum(1,3));
 //        segmentTree.update(0, 100);
--- SegmentTree.java
+++ SegmentTree.java
@@ -1,37 +1,37 @@
-
 import SegmentTreeExceptions.SegmentTreeOutOfBoundsException;
 import SegmentTreeExceptions.SizeLimitException;
+
 import java.util.Arrays;
 
 public class SegmentTree {
-    static int size;
     static final int SIZE_LIMIT = 100000;
+    static int size;
     static int[] containerArray;
     static int[] baseArray;
 
-    SegmentTree(int[] nums) throws SizeLimitException, SegmentTreeOutOfBoundsException{
+    SegmentTree(int[] nums) throws SizeLimitException {
         // Checking if the size is within bounds
-        if(size > SIZE_LIMIT)
-            throw  new SizeLimitException();
+        if (size > SIZE_LIMIT)
+            throw new SizeLimitException();
 
-        this.size = nums.length;
-        this.baseArray = nums;
+        size = nums.length;
+        baseArray = nums;
         containerArray = new int[4 * size];
         Arrays.fill(containerArray, Integer.MIN_VALUE);
 
-        generateSegmentTree(containerArray, nums, 0, 0, size-1);
+        generateSegmentTree(containerArray, nums, 0, 0, size - 1);
     }
 
     private void generateSegmentTree(int[] containerArray, int[] nums, int pos, int leftBound, int rightBound) {
         // Base condition
-        if(leftBound == rightBound)
+        if (leftBound == rightBound)
             containerArray[pos] = nums[leftBound];
 
         else {
             int midBound = leftBound + (rightBound - leftBound) / 2;
 
             generateSegmentTree(containerArray, nums, 2 * pos + 1, leftBound, midBound);
-            generateSegmentTree(containerArray, nums, 2 * pos + 2, midBound+1,rightBound);
+            generateSegmentTree(containerArray, nums, 2 * pos + 2, midBound + 1, rightBound);
 
             containerArray[pos] = containerArray[2 * pos + 1] + containerArray[2 * pos + 2];
         }
@@ -43,28 +43,28 @@ public class SegmentTree {
 
     public void display() {
         int first = 0;
-        while(containerArray[first] != Integer.MIN_VALUE)
+        while (containerArray[first] != Integer.MIN_VALUE)
             first++;
 
         System.out.println(Arrays.toString(Arrays.copyOfRange(containerArray, 0, first)));
     }
 
     public void update(int index, int newValue) throws SegmentTreeOutOfBoundsException {
-        if(index < 0 || index >= size)
+        if (index < 0 || index >= size)
             throw new SegmentTreeOutOfBoundsException("Exception : Update Index is out of bounds");
 
         int differenceInValue = newValue - baseArray[index];
         baseArray[index] = newValue;
 
         // Since any update will begin from root, we pass 0 in pos
-        updateHelper(0, size-1, index, differenceInValue, 0);
+        updateHelper(0, size - 1, index, differenceInValue, 0);
     }
 
     private void updateHelper(int leftBound, int rightBound, int index, int differenceInValue, int pos) {
-        if(index < leftBound || index > rightBound) return;
+        if (index < leftBound || index > rightBound) return;
 
         containerArray[pos] += differenceInValue;
-        if(leftBound != rightBound) {
+        if (leftBound != rightBound) {
             int mid = leftBound + (rightBound - leftBound) / 2;
 
             updateHelper(leftBound, mid, index, differenceInValue, 2 * pos + 1);
@@ -73,23 +73,25 @@ public class SegmentTree {
     }
 
     public int getRangeSum(int leftBound, int rightBound) throws SegmentTreeOutOfBoundsException {
-        if(leftBound < 0 || rightBound >= size || leftBound > rightBound)
+        if (leftBound < 0 || rightBound >= size || leftBound > rightBound)
             throw new SegmentTreeOutOfBoundsException("Exception : The Range is out of bounds.");
 
-        return getRangeSumHelper(0, size-1, leftBound, rightBound, 0);
+        return getRangeSumHelper(0, size - 1, leftBound, rightBound, 0);
     }
 
     private int getRangeSumHelper(int leftBound, int rightBound, int queryLeftBound, int queryRightBound, int pos) {
-        if(leftBound >= queryLeftBound || rightBound <= queryRightBound) return containerArray[pos];
+        // Case I : Segment completely inside range
+        if (leftBound >= queryLeftBound && rightBound <= queryRightBound) return containerArray[pos];
 
-        // Case of overlap
-        if(rightBound < queryLeftBound || leftBound > queryRightBound)  return 0;
+        // Case II : Segment completely outside range
+        if (rightBound < queryLeftBound || leftBound > queryRightBound) return 0;
 
+        // Case III : Overlapping segment
         int mid = leftBound + (rightBound - leftBound) / 2;
-//        int leftTreeOutput = ;
-//        int rightTreeOutput =;
 
-        return getRangeSumHelper(leftBound, mid, queryLeftBound, queryRightBound, 2 * pos + 1) +
-                getRangeSumHelper(mid + 1, rightBound, queryLeftBound, queryRightBound, 2 * pos + 2);
+        int leftSum = getRangeSumHelper(leftBound, mid, queryLeftBound, queryRightBound, 2 * pos + 1);
+        int rightSum = getRangeSumHelper(mid + 1, rightBound, queryLeftBound, queryRightBound, 2 * pos + 2);
+
+        return leftSum + rightSum;
     }
 }
